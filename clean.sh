fc4915c0d947d7e1eb8b424704f54af524a3c897
Kazem Forghani <kaz.forghani@gmail.com>
2024-01-25 20:16:18 +0330

    Added deleteing `*.out` files

--- clean.sh
+++ clean.sh
@@ -6,3 +6,10 @@ if ls *.o 1> /dev/null 2>&1; then
 else
     echo "No *.o files found."
 fi
+
+if ls *.out 1> /dev/null 2>&1; then
+    rm *.out
+    echo "Deleted all *.out files."
+else
+    echo "No *.out files found."
+fi
